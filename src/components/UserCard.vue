63d0ea10de8f7de22a0ce20f46a2da425a5d5774
manucarenjot <carenjotje@gmail.com>
2024-02-19 17:25:50 +0100

    requète axios fonctionnel

--- src/components/UserCard.vue
+++ src/components/UserCard.vue
@@ -1,9 +1,37 @@
 <template>
-  <h1>Hello world</h1>
+    <div v-for="(userData, index) in userData" :key="index">{{userData.gender}}</div>
 </template>
 
 <script>
+import axios from 'axios';
 export default {
+    data() {
+        return {
+            userData: [],
+        }
+
+    },
+
+    methods: {
+        getDataUser() {
+            axios.get('https://randomuser.me/api/?results=3')
+                .then(response=> {
+                    this.userData = response.data.results
+                    console.log(response.data.results)
+                })
+                    // en cas de réussite de la requête
+                .catch(function (error) {
+                    // en cas d’échec de la requête
+               
+                    console.log(error);
+                });
+        }
+    },
+
+    mounted() {
+        this.getDataUser()
+
+    }
 
 }
 </script>
