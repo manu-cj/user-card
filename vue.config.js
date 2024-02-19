c48d7b18068b99f00f67d82985b3d582a22eea5b
manucarenjot <carenjotje@gmail.com>
2024-02-19 20:57:16 +0100

    test

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,6 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  publicPath: process.env.NODE_ENV === "production" ? "/user-card/" : "/",
+  publicPath: process.env.NODE_ENV === "production" ? "/https://github.com/manucarenjot/user-card/" : "/",
   transpileDependencies: true
+
 })
